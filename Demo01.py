85c618f9225927cac2bbe0fb0db911d536393643
lm18634826135 <67538617+lm18634826135@users.noreply.github.com>
2020-06-28 17:13:52 +0800

    上床文件
    第一次使用github上传文件

---
+++ Demo01.py
@@ -0,0 +1,24 @@
+
+# demo1
+# print("Hellow Python")
+# temp = input("不妨猜一下现在心里想的是那个数字")
+# guess = int(temp)
+# if guess == 8:
+#     print("猜对了")
+# else:
+#     print("猜错了")
+# print("游戏结束")
+
+
+
+# demo2
+# name = input("请输入您的姓名：")
+# print("你好，"+name)
+
+# demo3
+number1 = input("请输入1到100之间的数字：")
+number = int(number1)
+if(1 <= number <= 100):
+    print("你好漂亮")
+else:
+    print("你好丑")
